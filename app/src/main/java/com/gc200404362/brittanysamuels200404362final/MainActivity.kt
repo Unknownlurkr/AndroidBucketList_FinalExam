e47459da7cb5806a20b6fecca4821967ee272724
Unknownlurkr <47453232+Unknownlurkr@users.noreply.github.com>
2021-12-16 12:04:59 -0500

    Initial Project Setup

---
+++ app/src/main/java/com/gc200404362/brittanysamuels200404362final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gc200404362.brittanysamuels200404362final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
