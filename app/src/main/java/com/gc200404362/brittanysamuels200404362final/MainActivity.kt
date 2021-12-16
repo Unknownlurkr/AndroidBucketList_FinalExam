de735afa8ee5df40b20e1120de7f3df014c094b0
Unknownlurkr <47453232+Unknownlurkr@users.noreply.github.com>
2021-12-16 16:27:08 -0500

    final commit

--- app/src/main/java/com/gc200404362/brittanysamuels200404362final/MainActivity.kt
+++ app/src/main/java/com/gc200404362/brittanysamuels200404362final/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gc200404362.brittanysamuels200404362final.databinding.ActivityMainBinding
-
+//THANK YOU :)
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
