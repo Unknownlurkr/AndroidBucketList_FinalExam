5db1b1e34b55fd8857e8352c810a17a12207e63f
Unknownlurkr <47453232+Unknownlurkr@users.noreply.github.com>
2021-12-16 12:22:31 -0500

    Challenge 2 – Firebase configured

---
+++ app/src/main/java/com/gc200404362/brittanysamuels200404362final/SigninActivity.kt
@@ -0,0 +1,61 @@
+package com.gc200404362.brittanysamuels200404362final
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        // Choose authentication providers
+        //stored as array list
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+// Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+
+            // Successfully signed in-RESULT_OK
+            val user = FirebaseAuth.getInstance().currentUser
+
+            //once user authenticated-create intent-pass to grid recylerview
+            val intent = Intent(this, MainActivity::class.java)
+            //pass in the user object
+            intent.putExtra("user", user)
+            //launches intent
+            startActivity(intent)
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Toast.makeText(this, "Sigin Failed ", Toast.LENGTH_LONG).show()
+            //naviagte back to the signin activity
+            startActivity(Intent(this, SigninActivity::class.java))
+        }
+    }
+}
